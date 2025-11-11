be9a67b51a2a49b8b3067cd2f98a27586ce2d198
watermelen63 <wumingxi63@gmail.com>
2025-11-12 00:26:00 +0800

    modified 11211073-2.py

--- Python/11211073-2.py
+++ Python/11211073-2.py
@@ -1,2 +1,15 @@
+def searchChar(a, s):
+    mid = len(a) // 2
+    left = [:a]
+    right = [a:]
+    
+
 ans, times = input(), int(input())
-print('*'*len(ans))
+say, ans_temp = '', '*' * len(ans)
+print(ans_temp)
+for i in range(times):
+    say = input()
+    try:
+        searchChar(ans, say)
+    except say not in ans:
+        print(ans_temp)
