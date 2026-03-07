a08ec42b16e610f4111b748dbdc204e24b1d10e4
watermelen63 <wumingxi63@gmail.com>
2026-03-08 00:14:54 +0800

    modified treeAnalysis.py

--- Python/treeAnalysis.py
+++ Python/treeAnalysis.py
@@ -1,1 +1,14 @@
-n = int(input())
+n, Sum, point, check = int(input()), 0, [], []
+father = 0
+
+for i in range(n):
+    Sum += (i + 1)
+
+for i in range(n):
+    arr = list(map(int, input().split()))
+    point.append(arr)
+    del arr[0]
+    check.extend(arr)
+
+father = Sum - sum(set(check))
+print(father)
