96c55e64bee62bd5849ca225e698a89a034c8a72
watermelen63 <wumingxi63@gmail.com>
2026-03-08 09:35:19 +0800

    modified treAnalysis

--- Python/treeAnalysis.py
+++ Python/treeAnalysis.py
@@ -1,6 +1,11 @@
-n, Sum, point, check = int(input()), 0, [], []
+﻿#基本輸入
+n, Sum, total_h, check = int(input()), 0, 0, []
+global point
+point = []
 father = 0
+point_val = {}
 
+#找父節點
 for i in range(n):
     Sum += (i + 1)
 
@@ -9,6 +14,31 @@ for i in range(n):
     point.append(arr)
     del arr[0]
     check.extend(arr)
+    if point[i]:
+        point_val.setdefault(f"{i + 1}", 1)
+    else:
+        point_val.setdefault(f"{i + 1}", 0)
 
-father = Sum - sum(set(check))
+#找H(T)
+def find_height(father):
+    global total_h
+    global point_val
+    ans = 0
+
+    if not point[father - 1]:
+        return 0
+    
+    max_h = 0
+    for child in point[father - 1]:
+        h = find_height(child)
+        if h > max_h:
+            max_h = h
+            
+    current_h = max_h + 1
+    total_h += current_h
+    return current_h
+
+father = Sum - sum(check)
 print(father)
+find_height(father)
+print(total_h)
